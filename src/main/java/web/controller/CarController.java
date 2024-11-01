073f93427fb751ab02fb4e40492bc289161cc281
mikkiice <mikkiice95@mail.ru>
2024-11-01 14:12:34 +0300

    r3

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,14 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import web.service.CarServiceImpl;
+import web.service.CarService;
 
 @Controller
 public class CarController {
-    private final CarServiceImpl carService;
+    private final CarService carService;
 
     @Autowired
-    public CarController(CarServiceImpl carService) {
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
